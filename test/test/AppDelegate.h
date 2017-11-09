58b255c06fa95bd66ba52a9f8e2bf23adb3ec55a
mc <2939914887@qq.com>
2017-11-09 01:00:17 -0800

    no message

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by mc on 2017/11/9.
+//  Copyright © 2017年 mc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
